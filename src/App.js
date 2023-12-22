9e56bf9cdabe4e3c8f0c472ec1d49bb43d07612f
yura2002 <ukujovda@gmail.com>
2023-12-22 17:06:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект ФИО";
+  const greeting = "Это тестовый проект Куйовда Ю. Ю.";
 
   console.log('This is App ');
   return (
